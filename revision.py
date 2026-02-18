bacb0522552dcde7ee2c31f8089cec8b2d248ceb
VISHAL <1316510155@kit.ac.in>
2026-02-18 11:18:03 +0530

    pattern programming practice

---
+++ revision.py
@@ -0,0 +1,44 @@
+name = "vishal Kumar"
+print(name ,"\n")
+
+print("pattern programming code \n")
+
+print("first pattern code \n")
+
+n= 5
+
+for i in range(1, n+1):print("*" * i)
+
+print("second pattern code \n ")
+
+n = 5 
+
+for i in range(n,0,-1):print("$" *i)
+
+print("third pattern code \n")
+
+n= 5
+
+for i in range(1,n+1):print(" " *(n-i)+"*" *i)
+
+print( "fourth Pattern code \n")
+
+n= 5
+for i in range( 1, n +1):
+
+    space = n-i
+    stars = 2*i -1
+    print(" " * space+"*" * stars) 
+
+print("inverted pyramid pattern")
+
+
+n= 5 
+
+for i in range(n,0,-1):
+    space = n-i
+
+    stars = 2*i -1 
+
+    print(" "* space+ "*" *stars)
+
