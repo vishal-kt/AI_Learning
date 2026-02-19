6f3df4eb3022ea7b237441c2f730ae16a3c3cbaf
VISHAL <1316510155@kit.ac.in>
2026-02-19 11:53:51 +0530

    some revision

---
+++ Pattern/Pattern.py
@@ -0,0 +1,19 @@
+print("first Pattern \n")
+
+n= 5 
+for  i in range(1, n+1):print("*" *i)
+print("\n")
+print("second Pattern \n")
+
+n= 5
+
+for i in range(n, 0, -1) : print("*" *i)
+
+print("\n")
+
+print("third pattern code \n")
+
+n=5
+
+for i in range(1, n+1):print(" " *(n-i)+ "*" *i)
+
